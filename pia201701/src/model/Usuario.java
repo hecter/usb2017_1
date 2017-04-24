5a888e91040a2d391adadf617bb668a8c5aa7c9c
admin <admin@10.0.134.203>
2017-04-24 15:32:04 -0500

    Added Getters and Setters

--- pia201701/src/model/Usuario.java
+++ pia201701/src/model/Usuario.java
@@ -10,12 +10,28 @@ package model;
  * @author Admin
  */
 public class Usuario {
-    String usuario;
-    String password; 
+    private String usuario;
+    private String password; 
  
     public Usuario(String usuario, String password) {
         this.usuario = usuario;
         this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
     }
     
 }
