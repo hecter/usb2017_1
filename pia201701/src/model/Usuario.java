65554ef3525dda079102bcd8ac56157c2b8ed376
admin <admin@gmail.com>
2017-04-24 15:44:39 -0500

    Se añade constructor vacio

--- pia201701/src/model/Usuario.java
+++ pia201701/src/model/Usuario.java
@@ -10,28 +10,16 @@ package model;
  * @author Admin
  */
 public class Usuario {
-    private String usuario;
-    private String password; 
+    String usuario;
+    String password; 
+
+    public Usuario() {
+    }
  
     public Usuario(String usuario, String password) {
         this.usuario = usuario;
         this.password = password;
     }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
+    
     
 }
