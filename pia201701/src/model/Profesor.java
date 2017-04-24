ea41f057d9862264eb892e622cb859c27955ae43
admin <admin@10.0.134.203>
2017-04-24 16:32:40 -0500

    Se agregan Getters y Setters

--- pia201701/src/model/Profesor.java
+++ pia201701/src/model/Profesor.java
@@ -12,4 +12,22 @@ package model;
 public class Profesor {
     private String nombre;
     private String apellido;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
 }
