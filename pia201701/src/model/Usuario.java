e693de363c52f62b00f08bdcd649d81369c8dc48
hec ter <Admin@10.0.134.200>
2017-04-24 15:24:16 -0500

    Creacion de Constructores

--- pia201701/src/model/Usuario.java
+++ pia201701/src/model/Usuario.java
@@ -12,5 +12,10 @@ package model;
 public class Usuario {
     String usuario;
     String password;
+
+    public Usuario(String usuario, String password) {
+        this.usuario = usuario;
+        this.password = password;
+    }
     
 }
